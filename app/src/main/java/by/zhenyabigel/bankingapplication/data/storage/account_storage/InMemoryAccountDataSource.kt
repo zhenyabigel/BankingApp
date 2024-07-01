bf4c7489fad88bacacc4b036e3ebcdab3d2909de
zhenyabigel <zhenya.bigel@mail.ru>
2024-07-01 16:23:59 +0300

    add calendar func

--- app/src/main/java/by/zhenyabigel/bankingapplication/data/storage/account_storage/InMemoryAccountDataSource.kt
+++ app/src/main/java/by/zhenyabigel/bankingapplication/data/storage/account_storage/InMemoryAccountDataSource.kt
@@ -1,29 +1,15 @@
 package by.zhenyabigel.bankingapplication.data.storage.account_storage
 
 import by.zhenyabigel.bankingapplication.data.model.Account
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
 import java.util.UUID
 
-@OptIn(DelicateCoroutinesApi::class)
 object InMemoryAccountDataSource : AccountDataSource {
     private val accounts = DefaultAccounts.associateBy { it.id }.toMutableMap()
     private val _accountFlow = MutableSharedFlow<Map<UUID, Account>>(1)
-    init {
-        GlobalScope.launch(Dispatchers.Default) {
-            while (isActive) {
-                _accountFlow.emit(accounts)
-            }
-        }
-    }
-
     override fun getAccounts(): Flow<List<Account>> {
         return _accountFlow.asSharedFlow().map { it.values.toList() }
     }
--- app/src/main/java/by/zhenyabigel/bankingapplication/data/storage/transaction_storage/InMemoryTransactionsDataSource.kt
+++ app/src/main/java/by/zhenyabigel/bankingapplication/data/storage/transaction_storage/InMemoryTransactionsDataSource.kt
@@ -1,32 +1,16 @@
 package by.zhenyabigel.bankingapplication.data.storage.transaction_storage
 
 import by.zhenyabigel.bankingapplication.data.model.Transaction
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
 import java.util.UUID
 
-@OptIn(DelicateCoroutinesApi::class)
 object InMemoryTransactionsDataSource : TransactionDataSource {
 
     private val transactions = DefaultTransactions.associateBy { it.id }.toMutableMap()
     private val _transactionFlow = MutableSharedFlow<Map<UUID, Transaction>>(1)
-
-    init {
-        GlobalScope.launch {
-            while (isActive) {
-                _transactionFlow.emit(transactions)
-                delay(2000L)
-            }
-        }
-    }
-
     override fun getTransactions(): Flow<List<Transaction>> {
         return _transactionFlow.asSharedFlow().map { it.values.toList() }
     }
